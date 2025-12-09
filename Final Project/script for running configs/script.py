35cf4ac31dd7ea97d0edd44b3c4e6e53963b180f
ahatfield-lab <ahatfield@colgate.edu>
2025-12-09 17:19:25 -0500

    Added final project stuff

---
+++ Final Project/script for running configs/script.py
@@ -0,0 +1,22 @@
+# nohup python -u /home/ahatfield/script.py > /home/ahatfield/script.log 2>&1 &
+# ps -p 415...
+# sbatch job.slurm
+# squeue -u ahatfield
+
+import os 
+
+#os.system("python /home/ahatfield/NLPScholar/main.py /home/ahatfield/finalproject/config/political_distilgpt_train_randomized.yaml")
+#os.system("python /home/ahatfield/NLPScholar/main.py /home/ahatfield/finalproject/config/political_distilbert_train_randomized.yaml")
+#os.system("python /home/ahatfield/NLPScholar/main.py /home/ahatfield/finalproject/config/nonpolitical_distilgpt_train_randomized.yaml")
+#os.system("python /home/ahatfield/NLPScholar/main.py /home/ahatfield/finalproject/config/nonpolitical_distilbert_train_randomized.yaml")
+#os.system("python /home/ahatfield/NLPScholar/main.py /home/ahatfield/finalproject/config/combined_distilgpt_train_randomized.yaml")
+#os.system("python /home/ahatfield/NLPScholar/main.py /home/ahatfield/finalproject/config/combined_distilbert_train_randomized.yaml")
+#os.system("python /home/ahatfield/NLPScholar/main.py /home/ahatfield/finalproject/config/evaluate_randomized.yaml")
+
+#os.system("python /home/ahatfield/NLPScholar/main.py /home/ahatfield/finalproject/config/political_distilgpt_train_nonrandomized.yaml")
+#os.system("python /home/ahatfield/NLPScholar/main.py /home/ahatfield/finalproject/config/political_distilbert_train_nonrandomized.yaml")
+#os.system("python /home/ahatfield/NLPScholar/main.py /home/ahatfield/finalproject/config/nonpolitical_distilgpt_train_nonrandomized.yaml")
+#os.system("python /home/ahatfield/NLPScholar/main.py /home/ahatfield/finalproject/config/nonpolitical_distilbert_train_nonrandomized.yaml")
+#os.system("python /home/ahatfield/NLPScholar/main.py /home/ahatfield/finalproject/config/combined_distilgpt_train_nonrandomized.yaml")
+#os.system("python /home/ahatfield/NLPScholar/main.py /home/ahatfield/finalproject/config/combined_distilbert_train_nonrandomized.yaml")
+#os.system("python /home/ahatfield/NLPScholar/main.py /home/ahatfield/finalproject/config/evaluate_nonrandomized.yaml")
